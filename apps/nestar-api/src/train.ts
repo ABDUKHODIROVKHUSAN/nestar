b2eb1e2dfe9269b9fbb3200b3b67e9b710b1442b
Khusan Abdukodirov <khusan@gmail.com>
2025-12-27 18:42:47 +0900

    feat: FinaL Logic and MIT TASK ZK

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -1,14 +1,29 @@
+// MIT TASK ZK
+
+function printNumbers(): void {
+  let count = 1;
+  const intervalId = setInterval(() => {
+    console.log(count);
+    count++;
+    if (count > 5) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
+printNumbers();
+
+
 // MIT TASK ZJ 1
 
-function reduceNestedArray(arr: any[]): number {
-  let sum = 0;
-  for (const item of arr) {
-    if (Array.isArray(item)) {
-      sum += reduceNestedArray(item); // recursive chaqiruv
-    } else {
-      sum += item;
-    }
-  }
-  return sum;
-}
-console.log(reduceNestedArray([1, [2, 3, [7]]]));
+// function reduceNestedArray(arr: any[]): number {
+//   let sum = 0;
+//   for (const item of arr) {
+//     if (Array.isArray(item)) {
+//       sum += reduceNestedArray(item); // recursive chaqiruv
+//     } else {
+//       sum += item;
+//     }
+//   }
+//   return sum;
+// }
+// console.log(reduceNestedArray([1, [2, 3, [7]]]));
