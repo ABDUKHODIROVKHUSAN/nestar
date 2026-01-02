27080d0a74774d5163fcb657847d8e1be32b172d
Khusan Abdukodirov <khusan@gmail.com>
2026-01-03 01:13:19 +0900

    feat: Authenticated and Authorized & Admin

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -21,7 +21,7 @@ export class MemberResolver {
     return this.memberService.login(input);
     }  
   
-
+    // Authenticated members only
   @Mutation(() => String)
   public async updateMember(): Promise<string> {
     console.log('Mutation: updateMember');
@@ -33,4 +33,17 @@ export class MemberResolver {
     console.log('Query: getMember');
     return this.memberService.getMember();
   }
-}
+
+    // Authorization: ADMIN
+  @Mutation(() => String)
+  public async getAllMembersByAdmin(): Promise<string> {
+    return this.memberService.getAllMembersByAdmin();
+  }
+
+  // Authorization: ADMIN
+  @Mutation(() => String)
+  public async updateMemberByAdmin(): Promise<string> {
+    console.log('Mutation: updateMemberByAdmin');
+    return this.memberService.updateMemberByAdmin();
+  }
+ }
