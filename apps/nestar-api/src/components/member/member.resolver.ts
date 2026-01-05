193911bba99f149b59188c1614a7b5cbc58d2aec
Khusan Abdukodirov <khusan@gmail.com>
2026-01-06 01:27:06 +0900

    feat: getMember defining part

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -10,6 +10,7 @@ import { MemberType } from '../../libs/enums/member.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/DTO/member/memberUpdate';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()  
 export class MemberResolver {
@@ -65,12 +66,14 @@ export class MemberResolver {
 
  
 
-  @Query(() => String)
-  public async getMember(): Promise<string> {
+  @Query(() => Member)
+  public async getMember(
+    @Args('memberId') input: string,
+  ): Promise<Member> {
     console.log('Query: getMember');
-    return this.memberService.getMember();
+    const targetId = shapeIntoMongoObjectId(input);
+    return this.memberService.getMember(targetId);
   }
-
     // Authorization: ADMIN
   @Mutation(() => String)
   public async getAllMembersByAdmin(): Promise<string> {
