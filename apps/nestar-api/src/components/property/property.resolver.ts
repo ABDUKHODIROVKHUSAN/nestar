adf1f20b2e388d437aac5e3ae33b3bcd6b80fe6e
Khusan Abdukodirov <khusan@gmail.com>
2026-01-16 19:21:58 +0900

    feat: Property liking logic is done

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -11,6 +11,8 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { WithoutGuard } from '../auth/guards/without.guard';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { PropertyUpdate} from '../../libs/DTO/property/property.update';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { Member } from '../../libs/DTO/member/member';
 
 @Resolver()
 export class PropertyResolver {
@@ -71,6 +73,17 @@ export class PropertyResolver {
       console.log('Query: getAgentProperties');
       return await this.propertyService.getAgentProperties(memberId, input);
   }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => Property)
+  public async likeTargetProperty(
+  @Args('propertyId') input: string,
+  @AuthMember('_id') memberId: ObjectId,
+  ): Promise<Property> {
+  console.log('Mutation: likeTargetProperty');
+  const likeRefId = shapeIntoMongoObjectId(input);
+  return await this.propertyService.likeTargetProperty(memberId, likeRefId);
+ }  
 
 
   /** ADMIN **/
