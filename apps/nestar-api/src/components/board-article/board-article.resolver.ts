c61e55501f5e7e87e60d6feb42b21edf7463aa6b
Khusan Abdukodirov <khusan@gmail.com>
2026-01-16 19:30:46 +0900

    feat: BoardArticle liking logic!

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -62,6 +62,17 @@ public async updateBoardArticle(
   return await this.boardArticleService.getBoardArticles(memberId, input);
 }
 
+@UseGuards(AuthGuard)
+  @Mutation(() => BoardArticle)
+  public async likeTargetBoardArticle(
+  @Args('articleId') input: string,
+  @AuthMember('_id') memberId: ObjectId,
+  ): Promise<BoardArticle> {
+  console.log('Mutation: likeTargetMember');
+  const likeRefId = shapeIntoMongoObjectId(input);
+  return await this.boardArticleService.likeTargetBoardArticle(memberId, likeRefId);
+}
+
 /** ADMIN */
 
 @Roles(MemberType.ADMIN)
